80ec9692d91b2cd345e50750fce961584ebf0862
Arthur Badin <akherousin94@gmail.com>
2022-04-13 21:38:03 +0600

    add 5 round game

--- script.js
+++ script.js
@@ -4,9 +4,6 @@ function computerPlay() {
   return selectedSign;
 }
 
-let playerSelection = "scissors";
-let computerSelection = computerPlay();
-
 function singleRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
 
@@ -15,18 +12,34 @@ function singleRound(playerSelection, computerSelection) {
 
   switch (playerSelection) {
     case "rock":
+      alert(
+        computerSelection === "paper"
+          ? "You lose! Paper beats Rock"
+          : "You win! Rock beats Scissors"
+      );
       return computerSelection === "paper"
         ? "You lose! Paper beats Rock"
         : "You win! Rock beats Scissors";
       break;
 
     case "paper":
+      alert(
+        computerSelection === "rock"
+          ? "You win! Paper beats Rock"
+          : "You lose! Scissors beat Paper"
+      );
       return computerSelection === "rock"
         ? "You win! Paper beats Rock"
         : "You lose! Scissors beat Paper";
       break;
 
     case "scissors":
+      alert(
+        computerSelection === "rock"
+          ? "You lose! Rock beats Scissors"
+          : "You win! Scissors beat Paper"
+      );
+      rock;
       return computerSelection === "rock"
         ? "You lose! Rock beats Scissors"
         : "You win! Scissors beat Paper";
@@ -34,4 +47,22 @@ function singleRound(playerSelection, computerSelection) {
   }
 }
 
-console.log(singleRound(playerSelection, computerSelection));
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt("Chose rock, paper or scissors");
+    let computerSelection = computerPlay();
+    let result = singleRound(playerSelection, computerSelection);
+    result.startsWith("You win!") ? playerScore++ : computerScore++;
+  }
+
+  alert(
+    playerScore > computerScore
+      ? `You win. Score is ${playerScore}:${computerScore}`
+      : `You lose. Score is ${playerScore}:${computerScore}`
+  );
+}
+
+game();
