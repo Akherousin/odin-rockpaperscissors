2e7750838d74938d1f8b933ef3c1750789c8f01f
Arthur Badin <akherousin94@gmail.com>
2022-04-30 16:29:45 +0600

    add simple ui

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+let playerScore = 0;
+let computerScore = 0;
+
+const gameContainer = document.querySelector(".game-container");
+const buttons = document.querySelectorAll(".btn");
+const currentScore = document.querySelector(".current-score");
+const currentRound = document.querySelector(".current-round");
+
+gameContainer.addEventListener("click", game);
+
 function computerPlay() {
   const signs = ["rock", "paper", "scissors"];
   const selectedSign = signs[Math.floor(Math.random() * signs.length)];
@@ -5,41 +15,23 @@ function computerPlay() {
 }
 
 function singleRound(playerSelection, computerSelection) {
-  playerSelection = playerSelection.toLowerCase();
-
   if (playerSelection === computerSelection)
     return `It's a draw. You both chose ${computerSelection}`;
 
   switch (playerSelection) {
     case "rock":
-      alert(
-        computerSelection === "paper"
-          ? "You lose! Paper beats Rock"
-          : "You win! Rock beats Scissors"
-      );
       return computerSelection === "paper"
         ? "You lose! Paper beats Rock"
         : "You win! Rock beats Scissors";
       break;
 
     case "paper":
-      alert(
-        computerSelection === "rock"
-          ? "You win! Paper beats Rock"
-          : "You lose! Scissors beat Paper"
-      );
       return computerSelection === "rock"
         ? "You win! Paper beats Rock"
         : "You lose! Scissors beat Paper";
       break;
 
     case "scissors":
-      alert(
-        computerSelection === "rock"
-          ? "You lose! Rock beats Scissors"
-          : "You win! Scissors beat Paper"
-      );
-      rock;
       return computerSelection === "rock"
         ? "You lose! Rock beats Scissors"
         : "You win! Scissors beat Paper";
@@ -47,22 +39,41 @@ function singleRound(playerSelection, computerSelection) {
   }
 }
 
-function game() {
-  let playerScore = 0;
-  let computerScore = 0;
+function game(e) {
+  if (!e.target.classList.contains("btn")) return;
 
-  for (let i = 0; i < 5; i++) {
-    let playerSelection = prompt("Chose rock, paper or scissors");
-    let computerSelection = computerPlay();
-    let result = singleRound(playerSelection, computerSelection);
-    result.startsWith("You win!") ? playerScore++ : computerScore++;
+  let playerSelection = e.target.value;
+  let computerSelection = computerPlay();
+  let result = singleRound(playerSelection, computerSelection);
+  currentRound.innerText = result;
+
+  if (result.startsWith("You win!")) {
+    playerScore++;
+  } else if (result.startsWith("You lose")) {
+    computerScore++;
   }
 
-  alert(
-    playerScore > computerScore
-      ? `You win. Score is ${playerScore}:${computerScore}`
-      : `You lose. Score is ${playerScore}:${computerScore}`
-  );
+  currentScore.innerText = `The current score is: ${playerScore} | ${computerScore}`;
+
+  if (isWinnerDetermined()) {
+    currentScore.innerText = theWinnerIs();
+    startNewGame();
+  } else return;
 }
 
-game();
+function isWinnerDetermined() {
+  return playerScore === 3 || computerScore === 3 ? true : false;
+}
+
+function theWinnerIs() {
+  return playerScore === 3
+    ? `You win. Score is ${playerScore}:${computerScore}`
+    : `You lose. Score is ${playerScore}:${computerScore}`;
+}
+
+function startNewGame() {
+  playerScore = 0;
+  computerScore = 0;
+  currentRound.innerText = "New round. Click a button";
+  // currentScore.innerText = "The current score is: 0 | 0";
+}
